dc5378593e12bbebf39babf837ba67aedcc0871f
NADISHAS <nadishas2745@gmail.com>
2025-08-20 21:58:52 +0530

    page routing

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites(){
+    return(
+        <>
+        <h1>No more favourite movies</h1>
+        <p>Start adding movies to favourite</p>
+        </>
+    )
+}
+
+export default Favourites;
